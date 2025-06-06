7c9b3d661c3f07a677f6a7222d39244d09fadda2
Wei Liu <13476241939@163.com>
2025-06-06 11:24:39 +0800

    商品分类信息 datax 脚本

---
+++ off_line_mall/datax/mysql_to_hdfs/script/product_category.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /etc/profile
+JSON=/data/lagoudw/script
+if [ -n "$1" ] ;then
+    do_date=$1
+else
+    do_date=`date -d "-1 day" +%F`
+fi
+
+# 创建目录
+hdfs dfs -mkdir -p /user/data/trade.db/product_category/dt=$do_date
+# 数据迁移
+python2 $DATAX_HOME/bin/datax.py -p "-Ddo_date=$do_date" $JSON/trade/product_category.json
