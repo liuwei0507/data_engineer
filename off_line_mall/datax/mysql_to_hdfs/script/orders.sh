679f92d5ae587eae2aaa95f0da17807e8a10e68d
Wei Liu <13476241939@163.com>
2025-06-06 15:10:21 +0800

    增量导入 datax 脚本

---
+++ off_line_mall/datax/mysql_to_hdfs/script/orders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /etc/profile
+JSON=/data/lagoudw/script
+if [ -n "$1" ] ;then
+    do_date=$1
+else
+    do_date=`date -d "-1 day" +%F`
+fi
+
+# 创建目录
+hdfs dfs -mkdir -p /user/data/trade.db/orders/dt=$do_date
+# 数据迁移
+python2 $DATAX_HOME/bin/datax.py -p "-Ddo_date=$do_date" $JSON/trade/orders.json
